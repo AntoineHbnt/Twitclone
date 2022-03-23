7e4a2aa0b0a23956c08ab9a44fb2cd0fc107f279
AntoineHbnt <49656644+AntoineHbnt@users.noreply.github.com>
2022-03-23 14:30:26 +0100

    some display fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,38 +7,32 @@ import TweetInput from "../components/TweetInput";
 import Log from "./Log";
 
 const Home = () => {
-
-  
-
   return (
     <main className="main-container">
       <div className="main-wrapper">
         <div className="content-container">
-          <div className="home-container">
-            <div className="home-wrapper">
-              <div className="home-header">
-                <div className="home-header-wrapper">
-                  <div className="left">
-                    <div className="title">
-                      <h2>
-                        <span>Accueil</span>
-                      </h2>
-                    </div>
+          <div className="page-content-wrapper">
+            <div className="home-header">
+              <div className="home-header-wrapper">
+                <div className="left">
+                  <div className="title">
+                    <h2>
+                      <span>Accueil</span>
+                    </h2>
                   </div>
-                  <div className="right">
-                    <div className="filter-btn">
-                      <img src="./img/icons/filter.svg" alt="" />
-                    </div>
+                </div>
+                <div className="right">
+                  <div className="filter-btn">
+                    <img src="./img/icons/filter.svg" alt="" />
                   </div>
                 </div>
               </div>
-              <div className="home-content">
-                <div className="home-content-wrapper">
-                  <TweetInput />
-                  <div className="break-line"></div>
-                  <Thread />
-                  
-                </div>
+            </div>
+            <div className="home-content">
+              <div className="home-content-wrapper">
+                <TweetInput />
+                <div className="break-line"></div>
+                <Thread />
               </div>
             </div>
           </div>
